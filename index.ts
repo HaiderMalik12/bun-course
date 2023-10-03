f81bd52168985c323a310c259ba23b73473fb1ca
HaiderMalik12 <haidermalik504@gmail.com>
2023-10-03 18:33:08 +0500

    adding routes

--- index.ts
+++ index.ts
@@ -3,8 +3,21 @@ import figlet from 'figlet'
 const server = Bun.serve({
     port: 3000,
     fetch(req){
-        const body = figlet.textSync("video")
-        return new Response(body)
+        const url = new URL(req.url)
+        if(url.pathname === '/'){
+            const body = figlet.textSync("video")
+            return new Response(body)
+        }
+        if(url.pathname === '/about'){
+            return new Response("About me!")
+        }
+
+        if(url.pathname === '/contact'){
+            return new Response("Contact Us!")
+        }
+
+        return new Response('404!')
+       
     }
 })
 
